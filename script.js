649bdacd5ed3cd417d262e803aa4eea79f10a4cb
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2020-03-04 17:26:40 +0300

    modified: script.js,style.css

--- script.js
+++ script.js
@@ -17,56 +17,81 @@ let navButtonOnClick = (item) => {
   })
 }
 
-
 navButton.forEach(navButtonOnClick);
 
-let getCards = () => {
-  const card = document.createElement('div');
-  const cardBack = document.createElement('div');
-  const cardFront = document.createElement('div');
-
-  card.classList.add('flip-card');
-  cardBack.classList.add('flip-card-back');
-  cardFront.classList.add('flip-card-front');
-
-  gameField.appendChild(card); 
-  card.appendChild(cardFront);
-  card.appendChild(cardBack);
-}
-
 let btnStartOnClick = () => {
+
   startMenu.remove();
   gameContainer.appendChild(gameField);
+  let numberOfCard;
 
   function getGameField() {
+
+    let getCards = () => {
+      const card = document.createElement('div');
+      const cardBack = document.createElement('div');
+      const cardFront = document.createElement('div');
     
+      card.classList.add('flip-card');
+      cardBack.classList.add('flip-card-back');
+      cardFront.classList.add('flip-card-front');
+    
+      gameField.appendChild(card); 
+      card.appendChild(cardFront);
+      card.appendChild(cardBack);
+
+    }
+
     if (btnSimple.classList.contains('checked')) {
       gameField.classList.add('three-card')
       for (i = 0; i < 3; i++) {
-        getCards()
+        getCards();
+        numberOfCard = 3;
       }
-      return
+      return numberOfCard;
     }
+
     else if (btnMedium.classList.contains('checked')) {
       gameField.classList.add('six-card')
       for (i = 0; i < 6; i++) {
-        getCards()
+        getCards();
+        numberOfCard = 6;
       }
-      return
+      return numberOfCard;
     }
+    
     else {
       gameField.classList.add('ten-card')
       for (i = 0; i < 10; i++) {
-        getCards()
+        getCards();
+        numberOfCard = 10;
       }
-      return
+      return numberOfCard;
     };
   }
 
   getGameField() 
 
+  const flipCard = document.querySelectorAll('.flip-card');
+  let randomCard = Math.floor(Math.random() * numberOfCard);
 
+  for (i = 0; i < numberOfCard; i++) {
+    if (i === randomCard) {
+      flipCard[i].firstElementChild.classList.add('winner');
+    } 
+  }
 
+  let flipCardOnClick = (item) => {
+    item.addEventListener('click', function() {
+      flipCard.forEach((el) => {
+        el.classList.remove('on-click');
+        this.classList.add('on-click');
+      })
+    })
+  }
+  
+  flipCard.forEach(flipCardOnClick);
 }
+ 
 
 btnStart.addEventListener('click', btnStartOnClick)
